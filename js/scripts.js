29333a7fe58402c5003b7de22ed7443f1ee7bb03
Marcelo Pinto <marcelos.mail@gmail.com>
2017-09-29 20:52:36 -0300

    Cards, position absolute and slide toggle

--- js/scripts.js
+++ js/scripts.js
@@ -193,3 +193,12 @@
 /*** Adds multiple items to cart : END ***/
 
 
+/*** Show and hide with animate : BEGIN ***/
+
+var couponButton = document.querySelector('#coupon-toggle');
+// When clicking the couponButton, toggle the coupon info visibility
+couponButton.addEventListener('click', function(e) {
+  $('.coupon-info').slideToggle();
+});
+
+/*** Show and hide with animate : END ***/
