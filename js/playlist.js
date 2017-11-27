82ff7baa617ceab3ca7ceaf1f2ed5d32e6bc70b2
marcelo-vp <marcelos.mail@gmail.com>
2017-11-27 00:45:29 -0200

    Added new song to playlist

--- js/playlist.js
+++ js/playlist.js
@@ -49,7 +49,9 @@ var playlist = new Playlist ();
 // Creating our songs
 var aQuestionOfHeaven = new Song ('A Question of Heaven','Iced Earth','11:42');
 var spaceMan = new Song ('Spaceman','The Killers','5:36');
+var starLight = new Song ('Starlight','Muse','4:52');
 
 // Adding the songs to the playlist
 playlist.add(aQuestionOfHeaven);
 playlist.add(spaceMan);
+playlist.add(starLight);
