a785d199d52cc4391b3c200e73f03340a5e4bec3
marcelo-vp <marcelos.mail@gmail.com>
2017-11-29 06:27:02 -0200

    Adding media.js

---
+++ js/media.js
@@ -0,0 +1,14 @@
+// Creating a media constructor object
+function Media (title, duration) {
+    this.title = title;
+    this.duration = duration;
+    this.isPlaying = false;
+}
+
+Media.prototype.play = function () {
+    this.isPlaying = true;
+}
+
+Media.prototype.stop = function () {
+    this.isPlaying = false;
+}
